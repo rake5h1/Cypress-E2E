16eda9486dd02a6bc2e0636e6ccf1133d3ad20c4
rake5h1 <rakeshkapgate1@gmail.com>
2023-10-28 17:19:09 +0530

    Changes MochaAwesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+
     },
   },
-  env:{
+  env: {
 
-    URL:'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
+    URL: 'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
   }
 });
