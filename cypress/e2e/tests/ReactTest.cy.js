0a6ed81cbef9895cc6afc081071c18a2d16b4b3c
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-07 22:36:47 +0530

    'changes'

--- cypress/e2e/tests/ReactTest.cy.js
+++ cypress/e2e/tests/ReactTest.cy.js
@@ -6,7 +6,8 @@ describe('React Test', () => {
 
 
         cy.visit(Cypress.env('URL'))
-        cy.get('h1#getting-started-with-create-react-app').should('be.visible').and('contain', 'Getting Started with Create React Apps')
+        cy.get('h1#getting-started-with-create-react-app').should('be.visible').and('contain', 'Getting Started with Create React App')
+        cy.get('div.container-lg a').eq(0).should('have.css', 'color', '#0366d6')
         cy.wait(5000)
     })
 
