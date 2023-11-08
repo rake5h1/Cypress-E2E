02deab0e4128dccaecd6b70a8c346c3f6a37ae09
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-08 08:31:29 +0530

    'changes'

--- cypress.config.js
+++ cypress.config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
     nonGlobalStepDefinitions: false,
 
 
-    stepDefinitions:'/cypress/integration'
+    integrationFolder: "cypress/integration"
   },
 
 
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,5 +3,6 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
 module.exports = (on, config) => {
 
   on('file:preprocessor', cucumber());
-
+  config.env.step_definitions = 'cypress/integration';
+  return config;
 };
