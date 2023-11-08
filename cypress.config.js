8ded5d5ee4902b40285c0276c7af302af69c7113
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-08 08:44:28 +0530

    'changes'

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
 
   },
   "cypress-cucumber-preprocessor": {
-    nonGlobalStepDefinitions: true,
+    nonGlobalStepDefinitions: false,
     integrationFolder: "cypress/integration"
   },
 
